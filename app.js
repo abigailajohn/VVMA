d4faeb37c5bd6c965e5a7959abfc010a87c4d406
unknown <praiseanthony18@gmail.com>
2025-02-05 23:06:24 +0100

    Added database connection and environment config

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 // app.js
 const express = require('express');
 const {specs, swaggerUi } = require('./swagger');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
@@ -10,6 +15,10 @@ app.get('/', (req, res) => {
   res.send('Hello, Swagger!');
 });
 
+const userRoutes = require('./routes/userRoutes');
+app.use('/', userRoutes);
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Swagger docs at http://localhost:${port}/api-docs`);
 });
