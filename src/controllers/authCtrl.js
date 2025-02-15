9c665578d1985bd2c39ce9605b7713e26b7cb9de
abigailajohn <abigailainyang@gmail.com>
2025-02-15 02:51:18 +0100

    Signed-off-by: abigailajohn <abigailainyang@gmail.com>
    added login endpoint
    fixed swaggerUi error
    added middleware for jwt verification

--- src/controllers/authCtrl.js
+++ src/controllers/authCtrl.js
@@ -1,4 +1,33 @@
 const db = require('../db/mysqldb');
+const jwt = require('jsonwebtoken');
+
+/**
+ * @swagger
+ * /api/auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     description: Creates a new user account.
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Bad request, invalid input
+ */
 
 const registerUser = async (req, res) => {
     try {
@@ -14,4 +43,77 @@ const registerUser = async (req, res) => {
     }
 };
 
-module.exports = { registerUser };
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: Login user
+ *     description: Authenticate a user and return a JWT token
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *               password:
+ *                 type: string
+ *                 format: password
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                     username:
+ *                       type: string
+ *                     email:
+ *                       type: string
+ *                     role:
+ *                       type: string
+ *       401:
+ *         description: Invalid credentials
+ *       500:
+ *         description: Server error
+ */
+
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const query = `SELECT * FROM users WHERE email = '${email}' AND password = '${password}'`;
+        const [users] = await db.execute(query);
+
+        if (users.length) {
+            const user = users[0];
+            const token = jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: '24h' });
+
+            res.json({ message: "Login successful", token });
+        } else {
+            res.status(401).json({ error: 'Invalid credentials' });
+        }
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+module.exports = { 
+    registerUser,
+    loginUser
+ };
