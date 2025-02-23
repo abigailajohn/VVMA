b860e4eb4598c3c41b4a8687ef666a1c33273f13
unknown <praiseanthony18@gmail.com>
2025-02-23 18:54:00 +0100

    updates

--- src/routes/resetPasswordRoutes.js
+++ src/routes/resetPasswordRoutes.js
@@ -3,10 +3,7 @@ const { requestPasswordReset, validateResetPassword } = require('../controllers/
 
 const router = express.Router();
 
-// POST /reset-password - Request password reset
 router.post('/reset-password', requestPasswordReset);
-
-// GET /reset-password - Validate or reset password link
 router.get('/reset-password', validateResetPassword);
 
 module.exports = router;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const { getUserById, updateUser, deleteUser, getUserByUsername } = require('../controllers/userCtrl');
+const { getUserById, updateUser, deleteUser } = require('../controllers/userCtrl');
 
 const router = express.Router();
 
-// GET /users/:id - Retrieve user profile (BOLA: leaks full data)
+
 router.get('/:id', getUserById);
-
-// PATCH /users/:id - Update user profile (mass assignment, IDOR)
 router.patch('/:id', updateUser);
-
-// DELETE /users/:id - Delete user account (BFLA: no confirmation)
 router.delete('/:id', deleteUser);
 
 module.exports = router;
