ab90ad937e660e9c14a211bd38b405c1c06ffdbf
unknown <praiseanthony18@gmail.com>
2025-02-23 18:44:45 +0100

    Completed user functions

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 //gathers all routes
 const express = require('express');
 const authRoutes = require('./auth'); 
+const userRoutes = require('./userRoutes');    
+const resetPasswordRoutes = require('./resetPasswordRoutes'); 
 
 const router = express.Router();
 
 router.use('/auth', authRoutes); 
+router.use('/users', userRoutes);   
+router.use('/', resetPasswordRoutes);      
 
 module.exports = router;
